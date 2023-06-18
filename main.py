5bc520b3a85df2f22111ee73c6f6e6854c582c3c
root <root@kali.kali>
2023-06-18 11:58:59 -0400

    complit get Member List

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ async def main(event):
         likns[1] = str(event.raw_text).split(" ")[2]
         members = await getMember(likns)
         links = linkmaker(likns)
-        await moveMember(members, likns[1])
+        await moveMember(members, links[1])
 
 #--------------------------------- check connect client ----------------------------------
 if client.is_connected():
