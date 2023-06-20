95f6f4b51e0d2e429714fe1d1a0b931ac1646f5a
root <root@kali.kali>
2023-06-20 10:09:18 -0400

    test singel move Member

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ async def moveMember(member, link, status):
             if status==0:
                 if await client(InviteToChannelRequest(
                     channel=link,
-                    users=['5548880104']
+                    users=['sisoc1']
                 )):
                     addMemberToDatabase(i)
             else:
