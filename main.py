910b183ba8c49239b4e9bf447c35384029c5be35
root <root@kali.kali>
2023-06-20 12:03:35 -0400

    test singel move Member

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ def addMemberToDatabase(id): #Complite
 
 async def getChatId():
     full = await client(GetFullChannelRequest('sisotest'))
-    print(full.chatfull.stringify())
+    print(full[0])
 
 async def moveMember(member, link, status):
     # for i in member:
